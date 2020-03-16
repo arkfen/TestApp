f7d9f17dd49a0594203c42e4a83da01c5ec34a07
Ark Fen <arkfen@gmail.com>
2020-03-16 15:10:25 +0300

    Cache re-factored and load implemented

--- Domain/Cache.cs
+++ Domain/Cache.cs
@@ -1,36 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using LifeItMusicApp.Models;
+using Newtonsoft.Json;
+using LifeItMusicApp.Local;
 
 namespace LifeItMusicApp.Domain
 {
     static class Cache
     {
+        private const string _artistsUrl = @"..\..\..\cache\artists.json";
+        private const string _albumsUrl = @"..\..\..\cache\albums.json";
         private static List<Artist> _artists;
         private static List<Album> _albums;
+        public static bool IsOn { get; private set; } = true;
 
         static Cache()
         {
-            _artists = GetArtists();
-            _albums = GetAlbums();
+            if (IsOn) LoadArtists();
+            if (IsOn) LoadAlbums();
         }
 
-        private static List<Artist> GetArtists()
+        private static void LoadArtists()
         {
-            List<Artist> artists = new List<Artist>();
-
-            throw new NotImplementedException();
-            return artists;
+            try
+            {
+                string json = File.ReadAllText(_artistsUrl);
+                _artists = (List<Artist>)JsonConvert.DeserializeObject(json, typeof(List<Artist>));
+            }
+            catch(Exception e)
+            {
+                TurnOff();
+            }
         }
 
-        private static List<Album> GetAlbums()
+        private static void LoadAlbums()
         {
-            List<Album> albums = new List<Album>();
-
-            throw new NotImplementedException();
-            return albums;
+            try
+            {
+                string json = File.ReadAllText(_albumsUrl);
+                _albums = (List<Album>)JsonConvert.DeserializeObject(json, typeof(List<Album>));
+            }
+            catch
+            {
+                TurnOff();
+            }
+            
         }
 
+        private static void TurnOff()
+        {
+            Console.WriteLine(Texts.CacheTurningOff);
+            IsOn = false;
+        }
+
+
+        // Test
+        public static void Test()
+        {
+            Console.WriteLine(_artists[0].Name);
+
+            return;
+        }
     }
 }
