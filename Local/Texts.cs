960e2d927bce7a631add9b889d2351519f95fa26
Ark Fen <arkfen@gmail.com>
2020-03-16 15:10:01 +0300

    Localization added

---
+++ Local/Texts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeItMusicApp.Local
+{
+    static class Texts
+    {
+        public const string CacheTurningOff = "Sorry, the offline mode is disactiveted because something went wrong. You can still use the app while online..."; 
+    }
+}
