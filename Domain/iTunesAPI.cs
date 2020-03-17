1021da9b75e3897ed16938a5f26e3f56d703d254
Ark Fen <arkfen@gmail.com>
2020-03-17 09:25:04 +0300

    Getting albums via API method implemented

--- Domain/iTunesAPI.cs
+++ Domain/iTunesAPI.cs
@@ -17,10 +17,10 @@ namespace LifeItMusicApp.Domain
         private const string _searchStringParam = "&term=";
 
         /// <summary>
-        /// Getting
+        /// Getting the list of artists via iTunes API using search string
         /// </summary>
-        /// <param name="searchString"></param>
-        /// <returns></returns>
+        /// <param name="searchString">Search String</param>
+        /// <returns>The list of Artists</returns>
         internal static List<Artist> GetArtists(string searchString)
         {
             // Trying to get data online and throwing exception or returning null if no success
@@ -41,7 +41,7 @@ namespace LifeItMusicApp.Domain
                 throw;
             }
             if (searchResults == null) return null;
-            // If there is not null results and no exceptions converting results to the internal Artists list format and return it
+            // If there results are not null and no exceptions converting results to the internal Artists list format and return it
             List<Artist> artists = new List<Artist>();
             if(searchResults != null && searchResults.resultCount > 0)
             {
@@ -58,9 +58,57 @@ namespace LifeItMusicApp.Domain
             return artists;
         }
 
+
+
+        /// <summary>
+        /// Getting the list of albums using iTunesAoi and Artist object
+        /// </summary>
+        /// <param name="artist">Artist whos albums needs to be retrived</param>
+        /// <returns>The lists of Albums</returns>
         internal static List<Album> GetAlbums(Artist artist)
         {
-            throw new NotImplementedException();
+            // Trying to get data online and throwing exception or returning null if no success
+            AlbumSearchResults searchResults = null;
+            string url = _urlMediaEntity + _entityValueAlbum + _searchStringParam + HttpUtility.UrlEncode(artist.Name);
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+                    response.EnsureSuccessStatusCode();
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    searchResults = JsonConvert.DeserializeObject<AlbumSearchResults>(json);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            if (searchResults == null) return null;
+            // If there results are not null and no exceptions converting results to the internal Album list format and return it
+            // !Attention => getting only those albums which have the same artistId property value as the target Artist object Id
+            List<Album> albums = new List<Album>();
+            if (searchResults != null && searchResults.resultCount > 0)
+            {
+                foreach (AlbumResultWrapper result in searchResults.results)
+                {
+                    if(result.artistId == artist.Id)
+                    {
+                        albums.Add(new Album
+                        {
+                            Id = result.collectionId,
+                            Name = result.artistName,
+                            ArtistId = result.artistId,
+                            ReleaseDate = result.releaseDate,
+                            Url = result.collectionViewUrl
+                        });
+                    }
+                }
+            }
+            return albums;
         }
+
+
+
     }
 }
